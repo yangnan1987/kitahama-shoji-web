9ba9cbf9affb6e5d34879d5426154c0fd1bc8594
yangnan1987 <yang.touca@gmail.com>
2026-04-04 00:07:26 +0900

    Polish UI and remove contact info

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,10 +1,7 @@
-/** サイト全体で参照する連絡先・表記（変更時はここを更新） */
+/** サイト全体で参照する表記（所在地・ドメイン等。変更時はここを更新） */
 export const SITE = {
   nameJa: '北浜商事株式会社',
   nameEn: 'Kitahama Shoji Co., Ltd.',
-  phoneDisplay: '06-4256-0564',
-  /** tel: 用（ハイフンなし推奨） */
-  phoneTel: '+81642560564',
   addressLines: ['〒542-0083', '大阪府大阪市中央区東心斎橋1-3-10-602'] as const,
   launched: '2026年1月',
   origin: 'https://kitahamashoji.com',
