4f8b4dc04e8d18b077929422a87a4c18804f0ddf
yangnan1987 <yang.touca@gmail.com>
2026-04-03 23:33:40 +0900

    Initial commit: Kitahama Shoji site

---
+++ src/constants/site.ts
@@ -0,0 +1,11 @@
+/** サイト全体で参照する連絡先・表記（変更時はここを更新） */
+export const SITE = {
+  nameJa: '北浜商事株式会社',
+  nameEn: 'Kitahama Shoji Co., Ltd.',
+  phoneDisplay: '06-4256-0564',
+  /** tel: 用（ハイフンなし推奨） */
+  phoneTel: '+81642560564',
+  addressLines: ['〒542-0083', '大阪府大阪市中央区東心斎橋1-3-10-602'] as const,
+  launched: '2026年1月',
+  origin: 'https://kitahamashoji.com',
+} as const;
